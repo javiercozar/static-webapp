c72b8920563b4c2f49eb9066e1df709a92d590ea
javier <javier.cozar@gmail.com>
2021-09-20 10:40:15 +0200

    restart app

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with javi
-          </a>
+          Learn React
+        </a>
       </header>
     </div>
   );
