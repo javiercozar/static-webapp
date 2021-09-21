b8d3bbc2ce2259e891ae93030fea0ed7004a6fad
javier <javier.cozar@gmail.com>
2021-09-21 08:47:17 +0200

    login and logout

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,31 +1,58 @@
 
-import { Button } from '@mui/material';
 import './App.css';
+import Link from '@mui/material/Link';
+import React, { useState, useEffect } from 'react';
 
 
-const LogginButton = ({Text, onClickHandler}) => {
-  return (<Button variant="contained" onClick={onClickHandler}>{Text}</Button>);
+const LogginButton = ({Text, Url, Redirect}) => {
+  return (<Link color="primary" href={`/${Url}?post_logout_redirect_uri=${Redirect}`}>{Text}</Link>);
 };
 
-const LoggoutButton = ({Text, onClickHandler}) => {
-  return (<Button variant="contained" onClick={onClickHandler}>{Text}</Button>);
+const LoggoutButton = ({Text, Url, Redirect}) => {
+  return (<Link color="primary" href={`/${Url}?post_logout_redirect_uri=${Redirect}`}>{Text}</Link>);
 };
 
 function App() {
+  const redirect = window.location.pathname;
 
-	const onLogginEventHandler = () => {
-		alert("Loggin");
-	};
+  const [userInfo, setUserInfo] = useState();
 
-	const onLoggoutEventHandler = () => {
-		alert("Loggout");
-	};
+  useEffect(() => {
+    (async () => {
+      setUserInfo(await getUserInfo());
+    })();
+  }, []);
+
+  async function getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
 
   return (
-    <div className="App">
-      <h1>Hello agents prime</h1>
-      <LogginButton Text="Loggin" onClickHandler={onLogginEventHandler}></LogginButton>
-			<LoggoutButton Text="Logout" onClickHandler={onLoggoutEventHandler}></LoggoutButton>
+    <div>
+      <div className="App">
+        <h1>Hello agents prime</h1>
+        <LogginButton Text="Loggin" Url="login" Redirect={redirect}></LogginButton>
+        <LoggoutButton Text="Logout" Url="logout" Redirect={redirect}></LoggoutButton>
+      </div>
+      <div>
+        {userInfo && (
+          <div>
+            <div className="user">
+              <p>Welcome</p>
+              <p>{userInfo && userInfo.userDetails}</p>
+              <p>{userInfo && userInfo.identityProvider}</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
